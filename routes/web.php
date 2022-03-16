16128cf29dd0736f5133c5b867c11eda682c9002
David Brown <007129090@coyote.csusb.edu>
2022-03-15 17:37:38 -0700

    needed to list controller

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::resource('/hwcategories', hwcategories::class);
+Route::resource('/hwcategories', hwcategoryController::class);
 
 
 
