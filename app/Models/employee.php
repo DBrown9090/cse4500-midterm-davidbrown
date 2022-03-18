cf010bc8b920d37667b2eb7a8dba71ad6173f593
David Brown <007129090@coyote.csusb.edu>
2022-03-18 11:57:34 -0700

    forgot to update the model too

--- app/Models/employee.php
+++ app/Models/employee.php
@@ -6,10 +6,16 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
+
 class employee extends Model
 {
     use HasFactory;
     use SoftDeletes;
+    use Database\Factories\Administration\EmployeeFactory;
 
     protected $fillable = ['Name', 'email', 'phone'];
+    protected static function newFactory()
+    {
+      return EmployeeFactory::new();
+    }
 }
