80e805dfffc83b849618a0f037195ee7febcfdd2
David Brown <007129090@coyote.csusb.edu>
2022-03-18 11:19:43 -0700

    seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,15 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $hwcatdata = [
+          ['Name'=>'Desktop'],
+          ['Name'=>'Laptop'],
+          ['Name'=>'Tablet'],
+          ['Name'=>'SmartPhone'],
+        ];
         // \App\Models\User::factory(10)->create();
+        foreach($hwcatdata as $hwc){
+          hwcategory::create($hwc);
+        }
     }
 }
